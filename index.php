621240301fad5b52e40151c1f657f1d2a0c29b78
Canaco <you@example.com>
2024-07-04 17:18:53 -0600

    foto

--- index.php
+++ index.php
@@ -37,59 +37,18 @@ if (!empty($_POST['username']) && !empty($_POST['password'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/style.css">
-    <style>
-        body {
-            background-color: #111D7E;
-            font-family: 'Arial', sans-serif;
-            color: white;
-        }
-
-        .container {
-            max-width: 400px;
-            margin: 0 auto;
-            margin-top: 100px;
-            padding: 20px;
-            background-color: #fff;
-            border-radius: 10px;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
-        }
-
-        .form-group {
-            margin-bottom: 20px;
-            color: black
-        }
-
-        .btn-primary {
-            background-color: #8cc4df;
-            border: none;
-            transition: background-color 0.3s;
-        }
-
-        .btn-primary:hover {
-            background-color: #0b1629;
-        }
-
-        .alert-danger {
-            background-color: #f44336;
-            color: #fff;
-            border: none;
-            border-radius: 5px;
-            padding: 10px;
-            margin-bottom: 10px;
-        }
-    </style>
 </head>
-<body>
+<body class="sesion">
     <header>
         <h1 class="titulo">
             <center>CANACO</center>
         </h1>
     </header>
-
-    <div class="container">
+    <img src="../css/img/logo.png">
+    <div class="container-s">
         <?php if (isset($errores) && count($errores) > 0) { ?>
             <?php foreach ($errores as $error) { ?>
-                <div class="alert alert-danger" role="alert"><?= $error ?></div>
+                <div class="alert alert-danger-s" role="alert"><?= $error ?></div>
             <?php } ?>
         <?php } ?>
 
@@ -102,20 +61,20 @@ if (!empty($_POST['username']) && !empty($_POST['password'])) {
                     <path d="M6.75 16a8.5 8.5 0 0 1 10.5 0" />
                 </svg>
             </h1>
-            <div class="form-group">
+            <div class="form-group-s">
                 <label for="username">Nombre del usuario:</label>
                 <input type="text" class="form-control" id="username" name="username">
             </div>
-            <div class="form-group">
+            <div class="form-group-s">
                 <label for="password">Contraseña:</label>
                 <input type="password" class="form-control" id="password" name="password">
             </div>
-            <button type="submit" class="btn btn-primary btn-block">Iniciar sesión</button>
+            <button type="submit" class="btn btn-primary-s btn-block">Iniciar sesión</button>
         </form>
         <br>
-        <div class="form-group text-center">
+        <div class="form-group-s text-center">
                 <p>¿No tienes usuario?
-                <a href="Usuarios/insertar.php" class="btn btn-primary">&nbsp;Registrate</a>
+                <a href="Usuarios/insertar.php" class="btn btn-primary-s">&nbsp;Registrate</a>
         </div>
     </div>
 </body>
